1a40ddaba1243c77db39ec3eaa82204e35ee2fb6
saiffeki5 <saiffeki5@gmail.com>
2023-12-04 11:57:59 +0100

    card-img-modif

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,48 +1,58 @@
-// Home.jsx
 import React, { useState,useRef  } from 'react';
-// import Card from './Card'; // Assurez-vous que le chemin est correct
-import { useNavigate } from 'react-router-dom';
 import BeautifulCards from '../../components/card/BeautifulCards';
 import CardForm from '../../components/cardForm/cardForm';
-
+import { Form, FormControl, Button } from 'react-bootstrap';
+import HEADER from '../../assets/cardsImages/HEADER1.png'
+import '../../assets/cardsImages/HomePageStyles.css' ; // Assurez-vous que le chemin est correct
+import SEARCH_ICON from '../../assets/cardsImages/Vector.svg'; // Importez votre icône SVG ici
 
 const homePage = () => {
-  const [annonces, setAnnonces] = useState([]);
   const [showForm, setShowForm] = useState(false);
-
-  const navigate = useNavigate();
   const formRef = useRef(null);  // Ajoutez une référence ici
 
-  
- 
-
-//   useEffect(() => {
-//     const fetchAnnonces = async () => {
-//       try {
-//         const response = await axios.get('http://localhost:8000/api/Annonces');
-//         setAnnonces(response.data);
-//       } catch (error) {
-//         console.error('Erreur lors de la récupération des annonces:', error);
-//       }
-//     };
-
-//     fetchAnnonces();
-//   }, []);
-const handleAddClick = () => {
+  const handleAddClick = () => {
     setShowForm(true); // Afficher le formulaire
     setTimeout(() => {
         formRef.current?.scrollIntoView({ behavior: 'smooth' });
       }, 100);
   };
 
+  
+
   return (
     <div>
-    <h1>Accueil</h1>
+      <div className='headerContainer'>
+      <h1 className='titleStyle'>MES ANNONCES</h1>
+      <FormControl
+                    type="text"
+                    placeholder="Recherche"
+                    className="searchBarStyle"
+                  />
+      <img src={HEADER} className='headerImageStyle' alt="Header"  />
+      
+      </div>
+          {/* <div className='headerContainerStyle'>
+            <img src={HEADER} className='headerImageStyle' alt="Header"  />
+            <div className="titleAndSearchContainer">
+              <h1 className='titleStyle'>MES ANNONCES</h1>
+              <div className="searchInputGroup">
+                <img src={SEARCH_ICON} className="searchIconStyle" alt="Search Icon" />
+
+                  <FormControl
+                    type="text"
+                    placeholder="Recherche"
+                    className="searchBarStyle"
+                    
+                  />
+              </div>
+
+          </div>
+   </div> */}
+
+
     <BeautifulCards /> {/* Ajout des cartes statiques */}
     <button onClick={handleAddClick}>Ajouter un événement</button>
     {showForm && <div ref={formRef}><CardForm /></div>}
-    {/* Décommentez la ligne suivante si vous affichez des annonces dynamiques */}
-    {/* {annonces.map((annonce, index) => (<Card key={index} {...annonce} />))} */}
   </div>
 );
 };
